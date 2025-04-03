f7792ea8fc96699b7b55ddc08102c547d919e302
AbbasRostami <abbas.cracker@gmail.com>
2025-04-03 19:29:29 +0330

    refactor

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,45 +1,48 @@
-import { users } from '@/app/lib/users';
-import { NextResponse } from 'next/server';
+import { users } from "@/app/lib/users";
+import { NextResponse } from "next/server";
+
+const generateUserId = () => Date.now();
 
 export async function POST(request: Request) {
-    try {
-        const { username, email, password, confirmPassword } = await request.json();
+  try {
+    const { username, email, password, confirmPassword } = await request.json();
 
-        if (!username || !email || !password || !confirmPassword) {
-            return NextResponse.json(
-                { error: 'لطفاً تمام فیلدها را پر کنید' },
-                { status: 400 }
-            );
-        }
-
-        if (password !== confirmPassword) {
-            return NextResponse.json(
-                { error: 'رمز عبور و تأیید رمز عبور مطابقت ندارند' },
-                { status: 400 }
-            );
-        }
-
-        const userExists = users.some((user: { email: string }) => user.email === email);
-
-        if (userExists) {
-            return NextResponse.json(
-                { error: 'این ایمیل قبلاً ثبت شده است' },
-                { status: 400 }
-            );
-        }
-
-        const newUser = { id: users.length + 1, username, email, password };
-        users.push(newUser);
-
-        return NextResponse.json(
-            { message: 'ثبت‌نام با موفقیت انجام شد', user: newUser },
-            { status: 201 }
-        );
-    } catch (error) {
-        console.error('Error during signup:', error);
-        return NextResponse.json(
-            { error: 'خطایی در پردازش درخواست رخ داد.' },
-            { status: 500 }
-        );
+    if (!username || !email || !password || !confirmPassword) {
+      return NextResponse.json(
+        { error: "لطفاً تمام فیلدها را پر کنید" },
+        { status: 400 }
+      );
     }
+
+    if (password !== confirmPassword) {
+      return NextResponse.json(
+        { error: "رمز عبور و تأیید رمز عبور مطابقت ندارند" },
+        { status: 400 }
+      );
+    }
+
+    const userExists = users.find((user) => user.email === email);
+
+    if (userExists) {
+      return NextResponse.json(
+        { error: "این ایمیل قبلاً ثبت شده است" },
+        { status: 400 }
+      );
+    }
+
+    const newUser = { id: generateUserId(), username, email, password };
+
+    users.push(newUser);
+
+    return NextResponse.json(
+      { message: "ثبت‌نام با موفقیت انجام شد", user: newUser },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error during signup:", error);
+    return NextResponse.json(
+      { error: "خطایی در پردازش درخواست رخ داد." },
+      { status: 500 }
+    );
+  }
 }
