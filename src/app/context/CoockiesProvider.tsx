4d4b1b3396d22ff852b91904e7af5a61af589902
AbbasRostami <abbas.cracker@gmail.com>
2025-04-03 18:49:41 +0330

    Add AuthProvider component for user authentication context and data fetching

---
+++ src/app/context/CoockiesProvider.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { getUserData } from "@/actions/isLogin";
+import { atom, useAtom } from "jotai";
+import { useSetAtom } from "jotai";
+import { useEffect } from "react";
+
+export const isLoggedInAtom = atom<boolean | null>(null);
+export const userAtom = atom<{ name?: string; email?: string } | null>(null);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const setIsLoggedIn = useSetAtom(isLoggedInAtom);
+  const setUser = useSetAtom(userAtom);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUserData();
+        setIsLoggedIn(data.isLoggedIn);
+        setUser(data.user);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        setIsLoggedIn(false);
+        setUser(null);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <>{children}</>;
+}
+
+export function useAuth() {
+  const [isLoggedIn] = useAtom(isLoggedInAtom);
+  const [user] = useAtom(userAtom);
+
+  return { isLoggedIn, user };
+}
