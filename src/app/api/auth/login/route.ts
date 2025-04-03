0e7531a09a145ecd53960f206a2f6385c5377d78
AbbasRostami <abbas.cracker@gmail.com>
2025-04-03 18:48:05 +0330

    Add authentication API routes for login and signup

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import { cookies } from 'next/headers';
+import { users } from '@/app/lib/users';
+
+export async function POST(request: Request) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: 'لطفاً تمام فیلدها را پر کنید.' },
+        { status: 400 }
+      );
+    }
+
+    const user = users.find((user) => user.email === email && user.password === password);
+
+    if (!user) {
+      return NextResponse.json(
+        { error: 'ایمیل یا رمز عبور اشتباه است.' },
+        { status: 401 }
+      );
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '1d' }
+    );
+
+    const cookieStore = await cookies(); 
+    cookieStore.set('token', token, {
+      httpOnly: true,  
+      secure: true,  
+      sameSite: 'lax', 
+      maxAge: 60 * 60 * 24 * 1, 
+      path: '/',     
+    });
+
+    return NextResponse.json({ message: 'ورود موفقیت‌آمیز بود' }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'خطایی در پردازش درخواست رخ داد.' },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,44 @@
+import { users } from '@/app/lib/users';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+    try {
+        const { username, email, password, confirmPassword } = await request.json();
+
+        if (!username || !email || !password || !confirmPassword) {
+            return NextResponse.json(
+                { error: 'لطفاً تمام فیلدها را پر کنید' },
+                { status: 400 }
+            );
+        }
+
+        if (password !== confirmPassword) {
+            return NextResponse.json(
+                { error: 'رمز عبور و تأیید رمز عبور مطابقت ندارند' },
+                { status: 400 }
+            );
+        }
+
+        const userExists = users.some((user: { email: string }) => user.email === email);
+
+        if (userExists) {
+            return NextResponse.json(
+                { error: 'این ایمیل قبلاً ثبت شده است' },
+                { status: 400 }
+            );
+        }
+
+        const newUser = { id: users.length + 1, username, email, password };
+        users.push(newUser);
+
+        return NextResponse.json(
+            { message: 'ثبت‌نام با موفقیت انجام شد', user: newUser },
+            { status: 201 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { error: 'خطایی در پردازش درخواست رخ داد.' },
+            { status: 500 }
+        );
+    }
+}
