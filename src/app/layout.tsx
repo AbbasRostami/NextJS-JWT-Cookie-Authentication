533d560da982150983bd3573f871716b81ffb7c1
AbbasRostami <abbas.cracker@gmail.com>
2025-04-03 18:49:34 +0330

    Add Header and Footer components with authentication context and toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header/Header";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/CoockiesProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <AuthProvider>
+        <Header />
+        <Toaster />
         {children}
+        <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
