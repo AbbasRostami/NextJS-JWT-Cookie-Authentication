7933389a184e543aacffeb9e4ffbb28598569c99
AbbasRostami <abbas.cracker@gmail.com>
2025-04-03 18:48:18 +0330

    Add middleware for token validation and redirect unauthorized access

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value;
+
+  if (!token) {
+    console.log('❌ Unauthorized access - No token found.');
+    
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/posts/:path*', '/users/:path*'],
+};
+
