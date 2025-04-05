989d1d11c75daba54a295ef860df1db0a02be149
AbbasRostami <abbas.cracker@gmail.com>
2025-04-05 03:36:08 +0330

    Format string literals consistently in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,18 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value;
+  const token = request.cookies.get("token")?.value;
 
   if (!token) {
-    console.log('❌ Unauthorized access - No token found.');
-    
-    return NextResponse.redirect(new URL('/login', request.url));
+    console.log("❌ Unauthorized access - No token found.");
+
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/posts/:path*', '/users/:path*'],
+  matcher: ["/posts/:path*", "/users/:path*"],
 };
-
